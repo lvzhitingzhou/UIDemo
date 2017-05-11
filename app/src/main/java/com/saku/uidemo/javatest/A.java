4afad0dffc46db2b5557475ad0d88805d4e5d60d
liuminmissa <liuminmissa@didichuxing.com>
2017-05-11 18:32:19 +0800

    aaaa

---
+++ app/src/main/java/com/saku/uidemo/javatest/A.java
@@ -0,0 +1,11 @@
+package com.saku.uidemo.javatest;
+
+public class A {
+//    A() {
+//        System.out.println("aaaa");
+//    }
+//
+    A(String param) {
+        System.out.println(param);
+    }
+}
---
+++ app/src/main/java/com/saku/uidemo/javatest/B.java
@@ -0,0 +1,11 @@
+package com.saku.uidemo.javatest;
+
+public class B extends A {
+//    B(){
+////        super();
+//    }
+
+    B(String p) {
+        super(p);
+    }
+}
